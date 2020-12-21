7f32651b565de28dd250090bd134608e76ece564
Jessica Murano <jamurano@yahoo.com>
2020-12-21 13:37:29 -0700

    UI Main Menu

---
+++ Surgery Project - XR Prototype/Assets/UI/Scripts/MainMenuUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void PlaySim()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void PlayClientSim()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void QuiteSim()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
