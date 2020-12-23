9355b86108298d88385f88a09fb9cf5364da952b
Gavin Leavitt <gavin.leavitt@gmail.com>
2020-12-23 12:55:26 -0700

    Adjusted scale, camera clipping, joints, etc.

---
+++ Surgery Project - XR Prototype/Assets/Scripts/OverrideClippingPlane.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class OverrideClippingPlane : MonoBehaviour
+{
+    public Camera cameraObj;
+    public float newNearClip;
+    public float newFarClip;
+
+    private void Awake()
+    {
+        cameraObj = GetComponent<Camera>();
+    }
+
+    private void Start()
+    {
+        cameraObj.nearClipPlane = newNearClip;
+        cameraObj.farClipPlane = newFarClip;
+    }
+}
