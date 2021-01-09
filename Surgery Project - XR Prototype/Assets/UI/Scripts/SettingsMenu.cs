37bf0a903b0a69543b81821e49c6a2990c922f9f
Gavin Leavitt <gavin.leavitt@gmail.com>
2021-01-09 14:19:34 -0700

    Finished resolution and quality dropdowns
    Switched to new input system, still need volume controls

--- Surgery Project - XR Prototype/Assets/UI/Scripts/SettingsMenu.cs
+++ Surgery Project - XR Prototype/Assets/UI/Scripts/SettingsMenu.cs
@@ -9,21 +9,35 @@ public class SettingsMenu : MonoBehaviour
     
     Resolution[] resolutions;
 
+    string[] qualityLevels;
+
     public Dropdown resolutionDropdown;
+    
+    public Dropdown qualityDropdown;
+
     private void Start()
     {
+        InitializeResolutionDropdown();
+    
+        InitializeQualityDropdown();
+
+    }
+
+    public void InitializeResolutionDropdown()
+    {
+        resolutionDropdown.ClearOptions();
+
         resolutions = Screen.resolutions;
         
-        resolutionDropdown.ClearOptions();
-        
-        List<string> options = new List<string>();
+        List<Dropdown.OptionData> newOptions = new List<Dropdown.OptionData>();
 
         int currentResolutionIndex = 0;
 
         for (int i = 0; i < resolutions.Length; i++)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
+            Dropdown.OptionData newOption = new Dropdown.OptionData();
+            newOption.text = resolutions[i].width + " x " + resolutions[i].height;
+            newOptions.Add(newOption);
 
             if (resolutions[i].width == Screen.currentResolution.width &&
                 resolutions[i].height == Screen.currentResolution.height)
@@ -32,15 +46,50 @@ public class SettingsMenu : MonoBehaviour
             }
         }
         
-        resolutionDropdown.AddOptions(options);
+        foreach (Dropdown.OptionData option in newOptions)
+        {
+            resolutionDropdown.options.Add(option);
+        }
+
         resolutionDropdown.value = currentResolutionIndex;
         resolutionDropdown.RefreshShownValue();
-
     }
 
-    public void SetResolution(int resolutionIndex)
+    public void InitializeQualityDropdown()
     {
-        Resolution resolution = resolutions[resolutionIndex];
+        qualityDropdown.ClearOptions();
+
+        qualityLevels = QualitySettings.names;
+        
+        List<Dropdown.OptionData> newOptions = new List<Dropdown.OptionData>();
+
+        int currentQualityIndex = 0;
+
+        for (int i = 0; i < qualityLevels.Length; i++)
+        {
+            Dropdown.OptionData newOption = new Dropdown.OptionData();
+            newOption.text = qualityLevels[i];
+            newOptions.Add(newOption);
+
+            if (i == QualitySettings.GetQualityLevel())
+            {
+                currentQualityIndex = i;
+            }
+        }
+        
+        foreach (Dropdown.OptionData option in newOptions)
+        {
+            qualityDropdown.options.Add(option);
+        }
+
+        qualityDropdown.value = currentQualityIndex;
+        qualityDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution()
+    {
+        Resolution resolution = resolutions[resolutionDropdown.value];
+        print(resolution);
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
@@ -49,9 +98,9 @@ public class SettingsMenu : MonoBehaviour
         audioMixer.SetFloat("Volume", volume);
     }
 
-    public void SetQuality(int qualityIndex)
+    public void SetQuality()
     {
-        QualitySettings.SetQualityLevel(qualityIndex);
+        QualitySettings.SetQualityLevel(qualityDropdown.value);
     }
 
     public void SetFullscreen(bool isFullscreen)
