e400de9c2fe0847f97ec6fa3ebde0896faa46ac2
Brandon <brandonjorgen@gmail.com>
2021-01-26 15:43:19 -0700

    pushing to look at older pushes

---
+++ Surgery Project - XR Prototype/Assets/Scripts/ReportingGrabInteractable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ReportingGrabInteractable : XRGrabInteractable
+{
+    public bool currentlyGrabbed;
+    public XRNode controller;
+    public InputDevice controllerDevice;
+
+    protected override void OnSelectEntered(XRBaseInteractor interactor)
+    {
+        base.OnSelectEntered(interactor);
+        currentlyGrabbed = true;
+        controller = selectingInteractor.gameObject.GetComponent<XRController>().controllerNode;
+        controllerDevice = InputDevices.GetDeviceAtXRNode(controller);
+    }
+
+    protected override void OnSelectExited(XRBaseInteractor interactor)
+    {
+        base.OnSelectExited(interactor);
+        currentlyGrabbed = false;
+    }
+}
