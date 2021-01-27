4c3f198313124200ed87fe0196f2748f7c87753e
Brandon <brandonjorgen@gmail.com>
2021-01-26 19:29:22 -0700

    XR is starting to get scary again
    currently working on tibial Guide Pin Drill

---
+++ Surgery Project - XR Prototype/Assets/Scripts/RigidbodyBehaviorChannel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class RigidbodyBehaviorChannel : MonoBehaviour
+{
+    private Rigidbody rb;
+    public GameAction channel;
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        channel.action += MoveForward;
+    }
+    
+    private void MoveForward()
+    {
+        rb.isKinematic = true;
+        rb.MovePosition(transform.position + (new Vector3(0, 0, 1) * (Time.deltaTime * 5)));
+    }
+}
