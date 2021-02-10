348a2d5bf5fff68bd26c2c2f32e0d6c8cfcb4b98
Brandon <brandonjorgen@gmail.com>
2021-02-09 22:45:00 -0700

    working towards disconnecting drill from Tibial Tunnel

---
+++ Surgery Project - XR Prototype/Assets/Scripts/EnabledOnTriggerEvent.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class EnabledOnTriggerEvent : MonoBehaviour
+{
+    private bool triggerEnabled;
+
+    public UnityEvent onTriggerEntered, onTriggerExited;
+    private Collider _collider;
+
+    private void Start()
+    {
+        _collider = GetComponent<Collider>();
+    }
+
+    private void FixedUpdate()
+    {
+        triggerEnabled = _collider.enabled;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (triggerEnabled)
+        {
+            onTriggerEntered.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (triggerEnabled)
+        {
+            onTriggerExited.Invoke();
+        }
+    }
+}
