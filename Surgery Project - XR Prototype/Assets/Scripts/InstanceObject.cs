b959839251d06ac088986450fd37d005c008e367
Brandon <brandonjorgen@gmail.com>
2021-02-12 11:46:57 -0700

    Drill removal and Transportal Guide in place + Femur guide pin progress

---
+++ Surgery Project - XR Prototype/Assets/Scripts/InstanceObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class InstanceObject : MonoBehaviour
+{
+    public GameObject prefab;
+
+    public void InstancePrefab()
+    {
+        Instantiate(prefab, transform.position, Quaternion.identity);
+    }
+}
