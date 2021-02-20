a2239fa3f02dc4bcd887dd7f9eb9ef353c912334
Brandon <brandonjorgen@gmail.com>
2021-02-20 13:36:37 -0700

    Restart/Exit Menu
    Also added the Building Model

---
+++ Surgery Project - XR Prototype/Assets/Scripts/CapsuleColliderBehavior.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(XRRig))]
+public class CapsuleColliderBehavior : MonoBehaviour
+{
+    private CharacterController obj;
+    private XRRig rig;
+
+    private void Start()
+    {
+        obj = GetComponent<CharacterController>();
+        rig = GetComponent<XRRig>();
+    }
+
+    private void FixedUpdate()
+    {
+        FollowCamera();
+    }
+
+    private void FollowCamera()
+    {
+        obj.height = rig.cameraInRigSpaceHeight;
+        Vector3 controllerCenter = transform.InverseTransformPoint(rig.cameraGameObject.transform.position);
+        obj.center = new Vector3(controllerCenter.x, obj.height / 2 + obj.skinWidth, controllerCenter.z);
+    }
+}
---
+++ Surgery Project - XR Prototype/Assets/Scripts/ResetExitMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetExitMenu : MonoBehaviour
+{
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+
+        if (Application.isEditor)
+        {
+            Debug.Log("Game has been closed but you're in editor");
+        }
+    }
+}
